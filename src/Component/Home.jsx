0ddaa7af00b0c21c952ca7aa238a0afaa3c43c95
OMI786I <sohrawardy1998@gmail.com>
2024-07-16 01:37:33 +0600

    popular menu items

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "./Banner";
 import Card from "./Card";
+import PopularMenu from "./PopularMenu";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <Card></Card>
+      <PopularMenu></PopularMenu>
     </div>
   );
 };
