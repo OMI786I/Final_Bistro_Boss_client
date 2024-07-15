4d29b189e9928ecf17aa934d6cc918509a4f62d1
OMI786I <sohrawardy1998@gmail.com>
2024-07-16 02:24:26 +0600

    created parallex

--- src/Component/Featured.jsx
+++ src/Component/Featured.jsx
@@ -3,14 +3,14 @@ import featuredImage from "../assets/assets/home/featured.jpg";
 import "../CSS/featured.css";
 const Featured = () => {
   return (
-    <div className="featured-item pt-8 my-20 ">
+    <div className="featured-item pt-8 my-20 bg-fixed ">
       <SectionTitle
         subHeading={"check it out"}
         heading={"Featured Item"}
       ></SectionTitle>
       <div className="md:flex justify-center items-center py-8 px-16 text-white">
         <div>
-          <img src={featuredImage}></img>
+          <img className="" src={featuredImage}></img>
         </div>
         <div className="md:ml-10">
           <p>Aug 20,2029</p>
@@ -24,7 +24,9 @@ const Featured = () => {
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
             culpa qui officia deserunt mollit anim id est laborum.
           </p>
-          <button className="btn btn-outline">Order Now</button>
+          <button className="btn btn-outline border-0 border-b-4 ">
+            Order Now
+          </button>
         </div>
       </div>
     </div>
