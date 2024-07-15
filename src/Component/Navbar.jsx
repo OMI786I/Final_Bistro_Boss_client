232a2f263b8ce5b472e7007271b170ddc5f8c357
OMI786I <sohrawardy1998@gmail.com>
2024-07-15 22:34:25 +0600

    added fixed navbar

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
     <div>
-      <div className="navbar bg-base-100">
+      <div className="navbar bg-base-100 fixed z-40 max-w-screen-xl bg-opacity-30 text-white bg-black">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
