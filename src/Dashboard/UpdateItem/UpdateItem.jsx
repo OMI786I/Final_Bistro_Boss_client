95a91f042d3b444ddd3b4b7885ff98470f512603
OMI786I <sohrawardy1998@gmail.com>
2024-08-22 14:15:28 +0600

    update data to mongodb

--- src/Dashboard/UpdateItem/UpdateItem.jsx
+++ src/Dashboard/UpdateItem/UpdateItem.jsx
@@ -36,9 +36,9 @@ const UpdateItem = () => {
       };
       const menuRes = await axiosSecure.patch(`/menu/${_id}`, menuItem);
       console.log(menuRes.data);
-      if (menuRes.data.insertedId) {
+      if (menuRes.data.modifiedCount > 0) {
         //success popup
-        reset();
+        // reset();
         Swal.fire({
           position: "top-end",
           icon: "success",
@@ -81,7 +81,7 @@ const UpdateItem = () => {
                   <span className="label-text">category</span>
                 </label>
                 <select
-                  defaultValue={"category"}
+                  defaultValue={category}
                   {...register("category")}
                   className="select select-bordered w-full "
                 >
